3b19f8140c774d8c91b899b4d9715653f95dc260
msomierick <erickmbwana@gmail.com>
2024-12-20 14:31:17 +0300

    Drop array.fill polyfill

--- src/app.js
+++ src/app.js
@@ -78,41 +78,6 @@ if (canUseDOM) {
   window.SmilesDrawer = SmilesDrawer;
 }
 
-
-if (!Array.prototype.fill) {
-  Object.defineProperty(Array.prototype, "fill", {
-    value: function (value) {
-      if (this == null) {
-        throw new TypeError("this is null or not defined");
-      }
-
-      const O = Object(this);
-      const len = O.length >>> 0;
-      const start = arguments[1];
-      const relativeStart = start >> 0;
-      const k =
-        relativeStart < 0
-          ? Math.max(len + relativeStart, 0)
-          : Math.min(relativeStart, len);
-
-      const end = arguments[2];
-      const relativeEnd = end === undefined ? len : end >> 0;
-
-      const final =
-        relativeEnd < 0
-          ? Math.max(len + relativeEnd, 0)
-          : Math.min(relativeEnd, len);
-
-      while (k < final) {
-        O[k] = value;
-        k++;
-      }
-
-      return O;
-    },
-  });
-}
-
 const input = document.getElementById("input");
 const debugCheckbox = document.getElementById("debug");
 const bondThicknessInput = document.getElementById("bondThickness");
